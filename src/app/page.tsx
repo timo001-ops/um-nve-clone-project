fb3797710b056f0b510bc8ceed452e76f21d258a
Luca Blancato <blancato@esteco.com>
2025-02-10 13:37:02 +0100

    progress to um25 page selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,33 +18,67 @@ export default function Home() {
         <div className="soul-grid-item-8/12-span both-um25-container">
           <div className="soul-grid soul-grid--horizontal">
             <div className="soul-grid-item-6/12-span um25-india-section">
-              <div className="">
-                <Image
-                  src={detailimg}
-                  alt={""}
-                  className="detail-um25-image-properties"
-                ></Image>
+              <div className="um25-india-content">
+                <div className="">
+                  <Image
+                    src={detailimg}
+                    alt={""}
+                    className="detail-um25-image-properties"
+                  ></Image>
+                </div>
+                <div className="">
+                  <h3 className="soul-heading soul-heading--h3 soul-font-size-3-xl um25-india-title">
+                    ESTECO <br></br> USERS&apos; MEETING <br></br>
+                    <b>
+                      INDIA <br></br>2025
+                    </b>
+                  </h3>
+                  <div className="um25-india-subtitle">
+                    <span>
+                      <b>
+                        Sheraton Grand Pune Bund,
+                        <br></br> Grand Hotel,
+                      </b>
+                      <br></br> Pune, India
+                    </span>
+                  </div>
+                </div>
               </div>
               <div>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
-                voluptatibus tenetur aut iusto beatae consectetur, dolores
-                maiores, eaque delectus ipsa totam pariatur magnam excepturi.
-                Eum quaerat dolorem esse sint laboriosam?
+                <button className="soul-button soul-button--default soul-button--fill soul-button--comfortable um25-india-button">
+                  Visit website
+                </button>
               </div>
             </div>
-            <div className="soul-grid-item-6/12-span">
-              <div className="">
-                <Image
-                  src={detailimg}
-                  alt={""}
-                  className="detail-um25-image-properties"
-                ></Image>
+            <div className="soul-grid-item-6/12-span um25-NA-section">
+              <div className="um25-NA-content">
+                <div className="">
+                  <Image
+                    src={detailimg}
+                    alt={""}
+                    className="detail-um25-image-properties"
+                  ></Image>
+                </div>
+                <div className="">
+                  <h3 className="soul-heading soul-heading--h3 soul-font-size-3-xl um25-NA-title">
+                    ESTECO <br></br> USERS&apos; MEETING <br></br>
+                    <b>NORTH AMERICA 2025</b>
+                  </h3>
+                  <div className="um25-NA-subtitle">
+                    <span>
+                      <b>
+                        Sheraton Grand Pune Bund,
+                        <br></br> Grand Hotel,
+                      </b>
+                      <br></br> Pune, India
+                    </span>
+                  </div>
+                </div>
               </div>
               <div>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
-                voluptatibus tenetur aut iusto beatae consectetur, dolores
-                maiores, eaque delectus ipsa totam pariatur magnam excepturi.
-                Eum quaerat dolorem esse sint laboriosam?
+                <button className="soul-button soul-button--default soul-button--fill soul-button--comfortable um25-NA-button">
+                  Visit website
+                </button>
               </div>
             </div>
           </div>
@@ -53,3 +87,35 @@ export default function Home() {
     </div>
   );
 }
+
+// <div className=" um25-india-section">
+//                 <div className="">
+//                   <Image
+//                     src={detailimg}
+//                     alt={""}
+//                     className="detail-um25-image-properties"
+//                   ></Image>
+//                 </div>
+//                 <div className="">
+//                   <h3 className="soul-heading soul-heading--h3 soul-font-size-3-xl um25-india-title">
+//                     ESTECO <br></br> USERS&apos; MEETING <br></br>
+//                     <b>INDIA 2025</b>
+//                   </h3>
+//                   <div className="um25-india-subtitle">
+//                     <span>
+//                       <b>
+//                         Sheraton Grand Pune Bund,
+//                         <br></br> Grand Hotel,
+//                       </b>
+//                       <br></br> Pune, India
+//                     </span>
+//                     <br></br>
+//                     <span>
+//                       <b>March 5th 2025</b>
+//                     </span>
+//                   </div>
+//                 </div>
+//               </div>
+//               <button className="soul-button soul-button--default soul-button--fill soul-button--comfortable um25-india-button">
+//                 Visit website
+//               </button>
